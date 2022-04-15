fc3bc699818e17cb5a2ae7d45a97e12f5ea0599b
andrewsaputra <andrew.saputra@gmail.com>
2022-04-15 14:11:05 +0700

    Update install-basic-dev-softwares.sh

--- ubuntu-20/install-basic-dev-softwares.sh
+++ ubuntu-20/install-basic-dev-softwares.sh
@@ -7,23 +7,9 @@ sudo apt install -y python3-pip
 sudo -H pip3 install --upgrade pip
 sudo pip3 install virtualenv
 
-#for backward compatibility with some older projects which are not compatible with python3
-sudo apt -y install python2
-
 sudo apt install -y curl
 sudo apt install -y php7.4 php7.4-dev php7.4-curl
-sudo apt install -y openjdk-8-jdk
 sudo apt install -y openjdk-11-jdk
-sudo apt install -y ruby
+sudo apt install -y containerd
+sudo apt install -y docker.io
 sudo apt install -y alacarte
-
-
-# MongoDB 3.6
-# require custom installation for libssl1.0.0 which is last available by default on ubuntu 18.04
-# https://docs.mongodb.com/v3.6/tutorial/install-mongodb-on-ubuntu/
-echo "deb http://security.ubuntu.com/ubuntu bionic-security main" | sudo tee /etc/apt/sources.list.d/bionic.list
-wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt update
-sudo apt install -y libssl1.0.0
-sudo apt install -y mongodb-org
