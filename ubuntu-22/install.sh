a113be43fa6659e06773c0acf195596bc87fe026
Andrew Saputra <andrew.saputra@gmail.com>
2024-01-30 17:54:33 +0700

    add erlang installation

--- ubuntu-22/install.sh
+++ ubuntu-22/install.sh
@@ -71,6 +71,10 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt update
 sudo apt install -y postgresql
 
+# erlang etc (for VerneMQ compilation)
+sudo snap install erlang
+sudo apt install -y libsnappy-dev libssl-dev
+
 # Kubernetes
 curl -Lo /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install /tmp/kubectl /usr/local/bin/kubectl
